1e3d802f9771c9b1ce5836408e1d0f48260e26b6
xXMRK888YTXx <aksehchikov@gmail.com>
2022-11-19 15:34:10 +0300

    [Docs] Added description for interfaces

--- AdminReceiver/src/main/java/com/xxmrk888ytxx/adminreceiver/AdminCallbackProvider.kt
+++ AdminReceiver/src/main/java/com/xxmrk888ytxx/adminreceiver/AdminCallbackProvider.kt
@@ -1,5 +1,13 @@
 package com.xxmrk888ytxx.adminreceiver
 
+/**
+ * [Ru]
+ * Данный интерфейс предназначен для предоставления [AdminEventsCallback], ресиверу [AdminReceiver],
+ * для класс который наследуется [Application], должен реализовавыть этот интерфейс.
+    [En]
+This interface is intended to provide [AdminEventsCallback], for receiver [AdminReceiver],
+ * for a class that inherits from [Application], must implement this interface.
+ */
 interface AdminCallbackProvider {
     val adminEventsCallback:AdminEventsCallback
 }
--- AdminReceiver/src/main/java/com/xxmrk888ytxx/adminreceiver/AdminEventsCallback.kt
+++ AdminReceiver/src/main/java/com/xxmrk888ytxx/adminreceiver/AdminEventsCallback.kt
@@ -1,8 +1,17 @@
 package com.xxmrk888ytxx.adminreceiver
 
+import android.app.admin.DeviceAdminReceiver
 import android.content.Context
 import android.content.Intent
 
+/**
+ * [Ru]
+ * Данный интерфейс предстовляет, колбэки для метотов аналогичных в [DeviceAdminReceiver]
+ * и вызывает аналогичные по названию методы
+ * [En]
+ * This interface represents callbacks for methods similar to [DeviceAdminReceiver]
+ * and calls methods similar in name
+ */
 interface AdminEventsCallback {
     fun onAdminEnabled()
     fun onAdminDisabled()
