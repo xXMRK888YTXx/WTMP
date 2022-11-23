2429d05db76f454455e393f3fe49e038ed2bc299
xXMRK888YTXx <aksehchikov@gmail.com>
2022-11-23 16:15:52 +0300

    [Docs] Update comments

--- core/core-Android/src/main/java/com/xxmrk888ytxx/coredeps/Interfaces/Repository/CameraManager.kt
+++ core/core-Android/src/main/java/com/xxmrk888ytxx/coredeps/Interfaces/Repository/CameraManager.kt
@@ -2,6 +2,15 @@ package com.xxmrk888ytxx.coredeps.Interfaces.Repository
 
 import java.io.File
 
+/**
+ * [Ru]
+ * Данный интерфейс предназначен для создания фотографий с камеры устройства,
+ * предварительно необходимо запросить разрешение (android.permission.CAMERA)
+ *
+ * [En]
+ * This interface is designed to take photos from the device's camera,
+ * you must first request permission (android.permission.CAMERA)
+ */
 interface CameraManager {
     fun createPhoto(
         outputFile: File,
--- core/core-Android/src/main/java/com/xxmrk888ytxx/coredeps/Interfaces/Repository/UserActivityStats.kt
+++ core/core-Android/src/main/java/com/xxmrk888ytxx/coredeps/Interfaces/Repository/UserActivityStats.kt
@@ -2,6 +2,19 @@ package com.xxmrk888ytxx.coredeps.Interfaces.Repository
 
 import android.app.usage.UsageStats
 
+/**
+ * [Ru]
+ * Данный интерфейс предназначен для получение статистики использования всех приложений или конкретного
+ * за промежуток времени.
+ * Для этого нужно запросить разрешения (android.permission.PACKAGE_USAGE_STATS), для этого необходимо реалозовать
+ * метод [requestPackageUsageStatePermission]
+ * [En]
+ * This interface is designed to obtain statistics on the use of all applications or a specific
+ * for a period of time.
+ * To do this, you need to request permissions (android.permission.PACKAGE_USAGE_STATS), for this you need to implement
+ * method [requestPackageUsageStatePermission]
+ */
+
 interface UserActivityStats {
     fun requestPackageUsageStatePermission()
 
