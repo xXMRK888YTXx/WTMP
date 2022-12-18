fc6253e648edf02bec85658fa139a21827d50062
xXMRK888YTXx <aksehchikov@gmail.com>
2022-12-18 16:48:14 +0300

    [Docs] update docs

--- OpenAppTracker/src/main/java/com/xxmrk888ytxx/openapptracker/OpenAppTrackerParams.kt
+++ OpenAppTracker/src/main/java/com/xxmrk888ytxx/openapptracker/OpenAppTrackerParams.kt
@@ -12,16 +12,40 @@ data class OpenAppTrackerParams private constructor(
 
         fun build() : OpenAppTrackerParams = OpenAppTrackerParams(isActivityOnly,ignoreList)
 
+        /**
+         * [Ru]
+         * Параметр указывающий, что будут отслеживаться процессы, которые имеют activity
+         */
+        /**
+         * [En]
+         * Parameter indicating that processes that have activity will be monitored
+         */
         fun isActivityOnly(state:Boolean) : Builder {
             isActivityOnly = state
             return this
         }
 
+        /**
+         * [Ru]
+         * Устанавливает имя пакетов которые будут игнорироваться, при отслежевании
+         */
+        /**
+         * [En]
+         * Sets the name of the packages to be ignored when tracking
+         */
         fun setIgnoreList(ignorePackages: List<String>) : Builder {
             ignoreList = ignorePackages
             return this
         }
 
+        /**
+         * [Ru]
+         * Устанавливает имя пакетов которые будут игнорироваться, при отслежевании
+         */
+        /**
+         * [En]
+         * Sets the name of the packages to be ignored when tracking
+         */
         fun setIgnoreList(vararg ignorePackages:String) : Builder {
             setIgnoreList(ignorePackages.toList())
             return this
