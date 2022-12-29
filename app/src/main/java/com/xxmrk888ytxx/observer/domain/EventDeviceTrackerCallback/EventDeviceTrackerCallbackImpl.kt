eb8370c02a2b5183dbb89f809432d957ba6a6cf5
xXMRK888YTXx <aksehchikov@gmail.com>
2022-12-29 12:21:12 +0300

    [Feature] Connect appOpen Params into callback

--- app/src/main/java/com/xxmrk888ytxx/observer/domain/EventDeviceTrackerCallback/EventDeviceTrackerCallbackImpl.kt
+++ app/src/main/java/com/xxmrk888ytxx/observer/domain/EventDeviceTrackerCallback/EventDeviceTrackerCallbackImpl.kt
@@ -1,9 +1,11 @@
 package com.xxmrk888ytxx.observer.domain.EventDeviceTrackerCallback
 
-import android.util.Log
 import com.xxmrk888ytxx.coredeps.ApplicationScope
+import com.xxmrk888ytxx.coredeps.SharedInterfaces.Configs.AppOpenConfig.AppOpenConfigProvider
 import com.xxmrk888ytxx.coredeps.SharedInterfaces.Configs.SucceededUnlockTrackedConfig.SucceededUnlockTrackedConfigProvider
+import com.xxmrk888ytxx.coredeps.SharedInterfaces.PackageInfoProvider
 import com.xxmrk888ytxx.coredeps.SharedInterfaces.Repository.DeviceEventRepository
+import com.xxmrk888ytxx.coredeps.SharedInterfaces.Repository.TrackedAppRepository
 import com.xxmrk888ytxx.coredeps.models.DeviceEvent
 import com.xxmrk888ytxx.eventdevicetracker.EventDeviceTrackerCallback
 import com.xxmrk888ytxx.eventdevicetracker.EventDeviceTrackerParams
@@ -15,13 +17,42 @@ import javax.inject.Inject
 internal class EventDeviceTrackerCallbackImpl @Inject constructor(
     private val deviceEventRepository: DeviceEventRepository,
     private val succeededUnlockTrackedConfigProvider: SucceededUnlockTrackedConfigProvider,
-    private val handleEventUseCase: HandleEventUseCase
+    private val handleEventUseCase: HandleEventUseCase,
+    private val appOpenConfig: AppOpenConfigProvider,
+    private val trackedAppRepository: TrackedAppRepository,
+    private val packageInfoProvider: PackageInfoProvider
 ): EventDeviceTrackerCallback {
+
     override val params: EventDeviceTrackerParams
         get() = EventDeviceTrackerParams.Builder().setIgnoreList(ignoreList).build()
 
     override fun onOpenAppChanged(packageName: String) {
-        Log.d("MyLog",packageName)
+        ApplicationScope.launch {
+            val config by lazy {
+                appOpenConfig.config
+            }
+
+            if(!config.first().isTracked||!isTrackedPackageName(packageName)) return@launch
+
+            val eventId = deviceEventRepository.addEvent(
+                DeviceEvent.AppOpen(0,null,packageName,
+                    null,System.currentTimeMillis())
+            )
+
+            val appName = packageInfoProvider.getAppName(packageName) ?: packageName
+
+            handleEventUseCase.execute(
+                eventId,
+                isSendTelegramMessage = config.first().notifyInTelegram,
+                makePhoto = config.first().makePhoto,
+                joinPhotoToTelegramNotify = config.first().joinPhotoToTelegramNotify,
+                "Приложение $appName было открыто."
+            )
+        }
+    }
+
+    private suspend fun isTrackedPackageName(packageName: String): Boolean {
+        return packageName in trackedAppRepository.getAllTrackedPackageNames().first()
     }
 
     override fun onScreenOn() {
