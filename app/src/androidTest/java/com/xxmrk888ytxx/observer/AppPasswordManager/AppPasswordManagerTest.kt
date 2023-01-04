603b45333274195fe7de08066c6a406045b1759e
xXMRK888YTXx <aksehchikov@gmail.com>
2023-01-04 11:03:19 +0300

    [Test] Update Tests

--- app/src/androidTest/java/com/xxmrk888ytxx/observer/AppPasswordManager/AppPasswordManagerTest.kt
+++ app/src/androidTest/java/com/xxmrk888ytxx/observer/AppPasswordManager/AppPasswordManagerTest.kt
@@ -1,5 +1,6 @@
 package com.xxmrk888ytxx.observer.AppPasswordManager
 
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.xxmrk888ytxx.coredeps.SharedInterfaces.AppPassword.AppPasswordChanger
 import com.xxmrk888ytxx.coredeps.SharedInterfaces.AppPassword.AppPasswordProvider
 import com.xxmrk888ytxx.cryptomanager.CryptoManagerImpl
@@ -7,10 +8,13 @@ import com.xxmrk888ytxx.observer.BaseSettingsAppManagerTest
 import com.xxmrk888ytxx.observer.domain.AppPasswordManager.AppPasswordManager
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
+import org.junit.After
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4::class)
 internal class AppPasswordManagerTest : BaseSettingsAppManagerTest() {
 
     private val appPasswordManager = AppPasswordManager(SettingsAppManagerHolder.settingsAppManager,CryptoManagerImpl())
@@ -28,36 +32,77 @@ internal class AppPasswordManagerTest : BaseSettingsAppManagerTest() {
     @Before
     fun init() = runBlocking {
         try {
-            changer.setupAppPassword(null,testPassword)
+            changer.removePassword(testPassword)
+        }catch (_:Exception) {}
+    }
+
+    @After
+    fun clear() = runBlocking {
+        try {
+            changer.removePassword(testPassword)
         }catch (_:Exception) {}
     }
 
     @Test
-    fun checkValidPasswordExpectCheckingReturnsTrue() = runBlocking {
+    fun setupPasswordAndCheckThemExpectCheckPasswordReturnsTrue() = runBlocking {
+        Assert.assertEquals(false,provider.isPasswordSetup().first())
+        
+        changer.setupAppPassword(testPassword)
+        
         Assert.assertEquals(true,provider.isAppPassword(testPassword))
     }
 
     @Test
-    fun checkInValidPasswordExpectCheckingReturnsFalse() = runBlocking {
-        Assert.assertEquals(false,provider.isAppPassword(testPassword + "1"))
+    fun setupPasswordAndCheckIncorrectPasswordExpectCheckPasswordReturnsFalse() = runBlocking {
+        Assert.assertEquals(false,provider.isPasswordSetup().first())
+
+        changer.setupAppPassword(testPassword)
+
+        Assert.assertEquals(false,provider.isAppPassword(testPassword+"1"))
+    }
+    
+    @Test
+    fun checkPasswordStateExpectReturnsFalse() = runBlocking {
+        Assert.assertEquals(false,provider.isPasswordSetup().first())
     }
 
     @Test
-    fun checkPasswordSetupExpectReturnsTrue() = runBlocking {
+    fun setupAndCheckPasswordStateExpectReturnsTrue() = runBlocking {
+        changer.setupAppPassword(testPassword)
         Assert.assertEquals(true,provider.isPasswordSetup().first())
     }
+    
+    @Test(expected = IllegalStateException::class)
+    fun setupPasswordRepeatedlyExpectThrowException() = runBlocking {
+        changer.setupAppPassword(testPassword)
+        changer.setupAppPassword(testPassword)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun setupPasswordAndRemoveThisWithIncorrectPasswordExpectThrowException() = runBlocking {
+        changer.setupAppPassword(testPassword)
+
+        changer.removePassword(testPassword+"1")
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun checkPasswordWithoutThisSetupExpectThrowException(): Unit = runBlocking {
+        provider.isAppPassword(testPassword)
+    }
 
     @Test
-    fun setupNewPasswordExpectNewPasswordIsValid() = runBlocking {
-        val newPassword = "@2222"
+    fun addPasswordChangerThemAndRemoveExpectIsPasswordSetupReturnsFalse() = runBlocking {
+        val testPassword2 = testPassword + "2"
+        changer.setupAppPassword(testPassword)
         Assert.assertEquals(true,provider.isAppPassword(testPassword))
 
-        changer.setupAppPassword(testPassword,newPassword)
-
-        Assert.assertEquals(true,provider.isAppPassword(newPassword))
+        changer.removePassword(testPassword)
+        changer.setupAppPassword(testPassword2)
         Assert.assertEquals(false,provider.isAppPassword(testPassword))
+        Assert.assertEquals(true,provider.isAppPassword(testPassword2))
+        changer.removePassword(testPassword2)
 
-        changer.setupAppPassword(newPassword,testPassword)
-        Assert.assertEquals(true,provider.isAppPassword(testPassword))
+        Assert.assertEquals(false,provider.isPasswordSetup().first())
     }
+    
 }
