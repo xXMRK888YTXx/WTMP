c228e93514b62a0ce900914706035b79f7211dce
xXMRK888YTXx <aksehchikov@gmail.com>
2023-01-08 10:52:48 +0300

    [Feature] Add boot device action with app

--- app/src/main/java/com/xxmrk888ytxx/observer/domain/BootCallback/BootCallbackImpl.kt
+++ app/src/main/java/com/xxmrk888ytxx/observer/domain/BootCallback/BootCallbackImpl.kt
@@ -1,15 +1,47 @@
 package com.xxmrk888ytxx.observer.domain.BootCallback
 
-import com.xxmrk888ytxx.androidextension.LogcatExtension.logcatMessageD
+import android.annotation.SuppressLint
 import com.xxmrk888ytxx.bootreceiver.BootCallback
+import com.xxmrk888ytxx.coredeps.ApplicationScope
+import com.xxmrk888ytxx.coredeps.SharedInterfaces.Configs.AppState.AppStateProvider
+import com.xxmrk888ytxx.coredeps.SharedInterfaces.Configs.BootDeviceTrackedConfig.BootDeviceTrackedConfigProvider
+import com.xxmrk888ytxx.coredeps.SharedInterfaces.Repository.DeviceEventRepository
+import com.xxmrk888ytxx.coredeps.SharedInterfaces.ResourcesProvider
+import com.xxmrk888ytxx.coredeps.models.DeviceEvent
+import com.xxmrk888ytxx.observer.R
+import com.xxmrk888ytxx.observer.domain.UseCase.HandleEventUseCase
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 internal class BootCallbackImpl @Inject constructor(
-
+    private val bootDeviceTrackedConfigProvider: BootDeviceTrackedConfigProvider,
+    private val appStateProvider: AppStateProvider,
+    private val deviceEventRepository: DeviceEventRepository,
+    private val handleEventUseCase:HandleEventUseCase,
+    private val resourcesProvider: ResourcesProvider
 ) : BootCallback {
 
+    @SuppressLint("ResourceType")
     override fun onBootCompleted() {
-        logcatMessageD("Boot finish")
+        ApplicationScope.launch {
+            val config = bootDeviceTrackedConfigProvider.config.first()
+
+            if(!config.isTracked||!appStateProvider.isAppEnable.first()) return@launch
+
+            val eventId = deviceEventRepository.addEvent(
+                DeviceEvent.DeviceLaunch(0,System.currentTimeMillis())
+            )
+
+            handleEventUseCase.execute(
+                eventId = eventId,
+                isSendTelegramMessage = config.notifyInTelegram,
+                makePhoto = config.makePhoto,
+                joinPhotoToTelegramNotify = config.joinPhotoToTelegramNotify,
+                telegramMessageText = resourcesProvider.getString(R.string.Device_loaded)
+            )
+        }
+
     }
 
 }
