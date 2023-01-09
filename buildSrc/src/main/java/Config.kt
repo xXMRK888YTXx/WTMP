4227a0202e3fcc6e4649613b2c1ea55608c13161
xXMRK888YTXx <aksehchikov@gmail.com>
2023-01-09 13:01:32 +0300

    [Feature] Enable R8 for release

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -3,5 +3,5 @@ object Config {
     const val compileSdk = 33
     const val minSdk = 23
     const val packageName = "com.xxmrk888ytxx.observer"
-    const val isR8ProGuardEnableForRelease = false
+    const val isR8ProGuardEnableForRelease = true
 }
