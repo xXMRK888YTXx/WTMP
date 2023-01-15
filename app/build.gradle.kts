d61501fdd8975da1f3ef7d79ed6d5cbe41808135
xXMRK888YTXx <aksehchikov@gmail.com>
2023-01-15 11:00:58 +0300

    [Chore] Up app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = Config.packageName
         minSdk = Config.minSdk
         targetSdk = Config.compileSdk
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner =  "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
