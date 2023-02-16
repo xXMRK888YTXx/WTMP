41a874836828af31e68950c775a0bf88aff1fbee
xXMRK888YTXx <aksehchikov@gmail.com>
2023-02-16 13:09:17 +0300

    [Fix] Fix replace limit

--- Database/src/main/java/com/xxmrk888ytxx/database/Dao/AppOpenTimeLimitDao.kt
+++ Database/src/main/java/com/xxmrk888ytxx/database/Dao/AppOpenTimeLimitDao.kt
@@ -1,8 +1,9 @@
 package com.xxmrk888ytxx.database.Dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import androidx.room.Upsert
 import com.xxmrk888ytxx.database.Entity.AppOpenTimeLimitEntity
 
 @Dao
@@ -10,7 +11,7 @@ internal interface AppOpenTimeLimitDao {
     @Query("SELECT * FROM AppOpenTimeLimit WHERE packageName = :packageName")
     fun getTimeLimitForApp(packageName:String) : AppOpenTimeLimitEntity?
 
-    @Upsert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertLimit(appOpenTimeLimitEntity: AppOpenTimeLimitEntity)
 
     @Query("DELETE FROM AppOpenTimeLimit WHERE packageName = :packageName")
