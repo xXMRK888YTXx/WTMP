ae3365e8e3e2b11d503d7fcf05053d37a85ef727
xXMRK888YTXx <aksehchikov@gmail.com>
2023-03-19 15:39:07 +0300

    [Docs] Update docs

--- core/core-Android/src/main/java/com/xxmrk888ytxx/coredeps/SharedInterfaces/Paginator.kt
+++ core/core-Android/src/main/java/com/xxmrk888ytxx/coredeps/SharedInterfaces/Paginator.kt
@@ -1,5 +1,22 @@
 package com.xxmrk888ytxx.coredeps.SharedInterfaces
 
+/**
+ * [Ru]
+ * Интрерфейс для пагинатора данных принимает два обобщения
+ *
+ * KEY - ключ странцы
+ *
+ * VALUE - Возвращаемый тип
+ */
+
+/**
+ * [En]
+ * * The interface for the data paginator accepts two generalizations
+ *
+ * KEY - the key of the stranger
+ *
+ * VALUE - Return type
+ */
 interface Paginator<KEY,VALUE> {
 
     suspend fun load()
--- core/core-Android/src/main/java/com/xxmrk888ytxx/coredeps/SharedInterfaces/Repository/DeviceEventRepository.kt
+++ core/core-Android/src/main/java/com/xxmrk888ytxx/coredeps/SharedInterfaces/Repository/DeviceEventRepository.kt
@@ -33,6 +33,29 @@ interface DeviceEventRepository {
      */
     fun getEventInTimeSpan(start:Long,end:Long) : Flow<List<DeviceEvent>>
 
+    /**
+     * [Ru]
+     *
+     * Данный метод используется для пагинации получаемых данных
+     *
+     * @param page - запрашиваемая страница
+     *
+     * @param limit - размер страницы
+     *
+     * Нумерация страниц начинается с 0
+     */
+
+    /**
+     * [En]
+     *
+     * This method is used for pagination of the received data
+     *
+     * @param page - requested page
+     *
+     * @param limit - page size
+     *
+     * Page numbering starts from 0
+     */
     suspend fun getPagingData(page:Int,limit:Int) : List<DeviceEvent>
 
     /**
