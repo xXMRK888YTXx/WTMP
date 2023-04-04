c39d3b2535120e5c99c0eacbd7c12d1ea43b6752
xXMRK888YTXx <aksehchikov@gmail.com>
2023-04-04 13:51:00 +0300

    [Test] Fix mistakes in tests and fix dependency error in tests

--- core/core-Android/build.gradle.kts
+++ core/core-Android/build.gradle.kts
@@ -37,6 +37,12 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    packagingOptions {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+        resources.excludes.add("META-INF/*")
+    }
 }
 
 dependencies {
@@ -62,4 +68,13 @@ dependencies {
 
     implementation(Deps.Moshi.moshi)
     kapt(Deps.Moshi.moshiKaptPlugin)
+
+    //Instrumental Test
+    androidTestImplementation (Deps.InstrumentalTest.espresso)
+    androidTestImplementation (Deps.InstrumentalTest.testRunner)
+    androidTestImplementation (Deps.InstrumentalTest.testCore)
+    androidTestImplementation (Deps.InstrumentalTest.jUnit)
+    androidTestImplementation (Deps.InstrumentalTest.testRules)
+    androidTestImplementation(Deps.TestAndroid.MockkAndroid)
+    androidTestImplementation(Deps.TestAndroid.MockkAgent)
 }
