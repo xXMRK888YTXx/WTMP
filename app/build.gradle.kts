d9d34084864f477ca21de78d30123ea1a41715bd
xXMRK888YTXx <aksehchikov@gmail.com>
2023-05-29 09:53:32 +0300

    [Chore] Up app version to 1.2.1r(version code 19)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = Config.packageName
         minSdk = Config.minSdk
         targetSdk = Config.compileSdk
-        versionCode = 18
-        versionName = "1.2.0r"
+        versionCode = 19
+        versionName = "1.2.1r"
 
         testInstrumentationRunner =  "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
